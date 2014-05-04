1e2aab9583573e57d6c667f3bcbaa0654f16846a
Steven Zhan <steven@hypersun.com>
2014-05-04 18:25:38 +0800

    add lesson http collect

---
+++ http_collect.js
@@ -0,0 +1,24 @@
+var bl = require("bl");
+
+var http = require("http");
+
+http.get(process.argv[2], function(response){
+	response.pipe(bl(function(err, data){
+		console.log(data.length);
+		console.log(data.toString());
+	}));
+});
+
+
+// var http = require('http')
+// var bl = require('bl')
+
+// http.get(process.argv[2], function (response) {
+//   response.pipe(bl(function (err, data) {
+//     if (err)
+//       return console.error(err)
+//     data = data.toString()
+//     console.log(data.length)
+//     console.log(data)
+//   }))
+// })
