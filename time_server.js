eb6bc26903746d25c9960604ddf1491cc4592689
Steven Zhan <steven.zhan@everbridge.com>
2014-05-06 22:13:04 +0800

    finish time_server, didn't notice that it requires a new line character earlier

--- time_server.js
+++ time_server.js
@@ -5,9 +5,30 @@ var date = new Date();
 
 //var strDate = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
 
-var strDate = strftime("%Y-%m-%d %H:%M", new Date())
+var strDate = strftime("%Y-%m-%d %H:%M\n", new Date())
 
 var server = net.createServer(function (socket){
 	socket.end(strDate);
 });
 server.listen(process.argv[2]);
+
+// var net = require('net')
+    
+//     function zeroFill(i) {
+//       return (i < 10 ? '0' : '') + i
+//     }
+    
+//     function now () {
+//       var d = new Date()
+//       return d.getFullYear() + '-'
+//         + zeroFill(d.getMonth() + 1) + '-'
+//         + zeroFill(d.getDate()) + ' '
+//         + zeroFill(d.getHours()) + ':'
+//         + zeroFill(d.getMinutes())
+//     }
+    
+//     var server = net.createServer(function (socket) {
+//       socket.end(now() + '\n')
+//     })
+    
+//     server.listen(Number(process.argv[2]))
