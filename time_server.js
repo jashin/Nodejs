0996b466e595b8fed0ad84aca328644d9c52af7b
Steven Zhan <steven@hypersun.com>
2014-05-20 18:27:36 +0800

    learn you node - time server

--- time_server.js
+++ time_server.js
@@ -5,7 +5,7 @@ var date = new Date();
 
 //var strDate = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
 
-var strDate = strftime("%Y-%m-%d %H:%M", new Date())
+var strDate = strftime("%Y-%m-%d %H:%M", new Date());
 
 var server = net.createServer(function (socket){
 	socket.end(strDate);
